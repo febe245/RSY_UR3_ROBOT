74e68a561fd8845ecfe8bb3120dd0aa8856d5e97
febe245 <barking.felix@gmx.de>
2025-10-22 17:12:05 +0200

    add ur3erobot class

--- main.py
+++ main.py
@@ -1,33 +1,18 @@
 import sys
-import rtde_control  
-import rtde_receive
-import rtde_io
-
-import socket
-import time
-
-# --- Netzwerkkonfiguration ---
-ROBOT3_IP = "192.168.0.17"
-ROBOT4_IP = "192.168.0.3"
-
-robot3Control = rtde_control.RTDEControlInterface(ROBOT3_IP)
-robot4Control = rtde_control.RTDEControlInterface(ROBOT4_IP)
-
-robot3Receive = rtde_receive.RTDEReceiveInterface(ROBOT3_IP)
-robot4Receive = rtde_receive.RTDEReceiveInterface(ROBOT4_IP)
+from robot.UR3eRobotInitialisation import robot3, robot4
 
 
 
 def testMain():
 
-    posRobot3 = robot3Receive.getActualTCPPose()
+    posRobot3 = robot3.getActualTCPPose()
     posRobot3[0] = posRobot3[0] -0.01
     posRobot3[1] = posRobot3[1] -0.01
     posRobot3[2] = posRobot3[2] -0.01
     posRobot3[3] = posRobot3[3] -0.01
     posRobot3[4] = posRobot3[4] -0.01
     posRobot3[5] = posRobot3[5] -0.01
-    robot3Control.moveL(posRobot3)
+    robot3.moveL(posRobot3)
     
     posRobot4 = robot3Receive.getActualTCPPose()
     posRobot4[0] = posRobot4[0] -0.01
@@ -36,7 +21,7 @@ def testMain():
     posRobot4[3] = posRobot4[3] -0.01
     posRobot4[4] = posRobot4[4] -0.01
     posRobot4[5] = posRobot4[5] -0.01
-    robot4Control.moveL(posRobot4)
+    robot4.moveL(posRobot4)
     
     
     
